b51b1453051d5d3d67d82106a8e5d5f71cd0d94c
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2021-05-04 14:59:48 +0530

    Column added in entry list table

--- src/app/entry-list-table/entry-list-table.component.ts
+++ src/app/entry-list-table/entry-list-table.component.ts
@@ -28,7 +28,7 @@ export class EntryListTableComponent implements OnInit {
   showLoader = false;
   paymentDetails: PaymentModel[] = [];
   displayedColumns: string[] = ['sno', 'date', 'name', 'category', 'amount',
-    'mode_of_payment', 'pan_card', 'action', 'receipt-print'];
+    'mode_of_payment', 'pan_card', 'state', 'party_unit', 'location', 'action', 'receipt-print'];
   private dialog: any;
   startdate = new FormControl('');
   enddate = new FormControl('');
