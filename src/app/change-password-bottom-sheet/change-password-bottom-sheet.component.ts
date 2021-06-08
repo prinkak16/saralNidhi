4bc9b3f1a3a37d874c354ff6a7767fcbfa3c5c6a
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2021-06-08 13:42:25 +0530

    password field issue fixed

--- src/app/change-password-bottom-sheet/change-password-bottom-sheet.component.ts
+++ src/app/change-password-bottom-sheet/change-password-bottom-sheet.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {RestService} from '../services/rest.service';
 import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {UtilsService} from '../services/utils.service';
 
 @Component({
   selector: 'app-change-password-bottom-sheet',
@@ -16,13 +17,14 @@ export class ChangePasswordBottomSheetComponent implements OnInit {
   constructor(private bottomSheetRef: MatBottomSheetRef<ChangePasswordBottomSheetComponent>,
               @Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
               private restService: RestService, private snackBar: MatSnackBar,
+              public utilsService: UtilsService,
               private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.passwordForm = this.formBuilder.group({
-      password: new FormControl(null, [Validators.required]),
-      confirmPassword: new FormControl(null, [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.pattern(this.utilsService.passwordPattern)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.pattern(this.utilsService.passwordPattern)]),
       id: new FormControl(this.data.userId)
     });
   }
@@ -32,18 +34,19 @@ export class ChangePasswordBottomSheetComponent implements OnInit {
       this.snackBar.open('Password and Confirm password should match.', 'Okay', {
         duration: 3000
       });
+    } else {
+      this.restService.updatePassword(this.passwordForm.value).subscribe((reply: any) => {
+        this.snackBar.open('Password  Updated', 'Okay', {
+          duration: 3000
+        });
+        this.bottomSheetRef.dismiss();
+      }, (error: string) => {
+        this.snackBar.open((error) ? error
+          : 'Unable to update password at this moment', 'Okay', {
+          duration: 5000
+        });
+      });
     }
-    this.restService.updatePassword(this.passwordForm.value).subscribe((reply: any) => {
-      this.snackBar.open('Password  Updated', 'Okay', {
-        duration: 3000
-      });
-      this.bottomSheetRef.dismiss();
-    }, (error: string) => {
-      this.snackBar.open((error) ? error
-        : 'Unable to update password at this moment', 'Okay', {
-        duration: 5000
-      });
-    });
   }
 
 }
