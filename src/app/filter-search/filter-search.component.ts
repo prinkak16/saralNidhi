850a1d8922fb0c094abe871ad3b0cec750455d20
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2021-08-11 20:19:46 +0530

    minor bugs

--- src/app/filter-search/filter-search.component.ts
+++ src/app/filter-search/filter-search.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {saveAs} from "file-saver";
+import {RestService} from '../services/rest.service';
+import {MessageService} from '../services/message.service';
 
 
 @Component({
@@ -10,13 +13,16 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class FilterSearchComponent implements OnInit {
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
+  constructor(private router: Router, private restService: RestService,
+              private messageService: MessageService,
+              private formBuilder: FormBuilder) {
   }
 
   @Output() applyFilter = new EventEmitter<any>();
   @Input() query: any = null;
   filterForm: FormGroup = new FormGroup({});
   today =  new Date();
+  downloadCount = 1;
   ngOnInit(): void {
     this.filterForm = this.formBuilder.group({
       query: new FormControl(this.query),
@@ -35,4 +41,17 @@ export class FilterSearchComponent implements OnInit {
     this.filterForm.controls.query.setValue(null);
     this.getFilteredData();
   }
+
+  downloadList(): void {
+    this.restService.downloadTransactionList().subscribe((reply: any) => {
+      const mediaType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+      const blob = new Blob([reply], {type: mediaType});
+      const filename = `NidhiCollection.xlsx`;
+      saveAs(blob, this.downloadCount+filename);
+      this.downloadCount = this.downloadCount+1;
+    }, (error: any) => {
+      this.messageService.somethingWentWrong(error ? error : 'Error Downloading');
+    });
+  }
+
 }
