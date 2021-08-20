48b2bcce1bfd3ec15fc3e217a22932b1eaa7249e
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2021-08-20 12:11:14 +0530

    minor change

--- src/app/receipt-dialog/receipt-dialog.component.ts
+++ src/app/receipt-dialog/receipt-dialog.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, Inject, Optional } from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, OnInit, Inject, Optional} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {RestService} from '../services/rest.service';
 
 class UsersData {
