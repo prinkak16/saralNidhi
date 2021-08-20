e948f7e25f147cedafc83919ad97526386d4907a
rajnish-jarvis <rajnish.mishra@jarvis.consulting>
2021-08-20 12:03:21 +0530

    download progress display

--- src/app/receipt-dialog/receipt-dialog.component.ts
+++ src/app/receipt-dialog/receipt-dialog.component.ts
@@ -1,9 +1,6 @@
-import {Component, OnInit, Inject, Optional} from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {RestService} from '../services/rest.service';
-import {MessageService} from '../services/message.service';
-
-import {saveAs} from 'file-saver';
 class UsersData {
 }
 
@@ -18,35 +15,17 @@ export class ReceiptDialogComponent implements OnInit {
   receipt_data: any;
 
   constructor(
-    private restService: RestService,
-    private messageService: MessageService,
     public dialogRef: MatDialogRef<ReceiptDialogComponent>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: UsersData) {
     this.receipt_data = {...data};
   }
-
   ngOnInit(): void {
   }
-
   print(): void {
     window.print();
   }
-
   close(): void {
     this.dialogRef.close();
-  }
-
-  downloadReceipt(row: any): void {
-    this.dialogRef.close();
-    this.restService.downloadReceipt(row.id).subscribe((reply: any) => {
-      let filename = row.data.name.replace(' ', '_');
-      const mediaType = 'application/pdf';
-      const blob = new Blob([reply], {type: mediaType});
-      filename = filename + `-${(new Date()).toString().substring(0, 24)}.pdf`;
-      saveAs(blob, filename);
-    }, (error: any) => {
-      this.messageService.somethingWentWrong(error);
-    });
   }
 
 
