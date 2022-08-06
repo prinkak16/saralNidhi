a47ee3a5c7ef8bf108f06fb3819881c3353b6d9b
vikasJarvis <90310447+vikasJarvis@users.noreply.github.com>
2022-08-06 12:13:32 +0530

    Update Base.java

--- selenium/src/main/java/resources/Base.java
+++ selenium/src/main/java/resources/Base.java
@@ -1,10 +1,13 @@
 
+
 package resources;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -24,58 +27,63 @@ public class Base {
 
 	public WebDriver driver;
 	public String url;
+	public Properties prop;
 
 	public WebDriver initializeDriver() throws IOException {
 
-		Properties prop = new Properties();
-
+		prop = new Properties();
+		
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
-
+		
 		prop.load(fis);
-
+		
 		//String browserName = prop.getProperty("browser");
-
-		// following is for parameterized browser through maven command
+		
+		// following is for parameterized browser through maven command 
 		String browserName = System.getProperty("browser");
-
+		
 		System.out.println("browser name is :"+browserName);
 		url = prop.getProperty("url");
 
 		if (browserName.contains("chrome")) {
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Downloads\\chromedriver.exe");
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\chromedriver.exe");
-
+			
 		    WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
-
+			
 			HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 			chromePrefs.put("profile.default_content_settings.popups", 0);
 			chromePrefs.put("download.default_directory", System.getProperty("user.dir")+"\\downloadUserManagement");
 			options.setExperimentalOption("prefs", chromePrefs);
-
+			
+			
 			if(browserName.contains("headless")) {
 				options.addArguments("--headless");
 				options.addArguments("--window-size=1920,1080");
-        System.out.println("inside chrome headless....");
+				System.out.println("inside chrome headless....");
 			}
-
+			
 			driver = new ChromeDriver(options);
-
+		
 		}
 
 		else if (browserName.equals("edge")) {
 			//System.setProperty("webdriver.edge.driver", "C:\\Users\\PC\\Downloads\\msedgedriver.exe");
 			//System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\msedgedriver.exe");
-      System.out.println("inside edge");
+			
 			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
+			System.out.println("inside edge");
+			//driver = new EdgeDriver();
 			EdgeOptions options = new EdgeOptions();
-
+			
 			if(browserName.contains("headless")) {
+				
+				System.out.println("inside edge headless");
 				options.addArguments("--headless");
-				options.addArguments("--window-size=1920,1080");
+				options.addArguments("--window-size=1920,1080");	
 			}
-			//driver = new EdgeDriver(options);
+			driver = new EdgeDriver(options);
 		}
 
 		else if (browserName.equals("firefox")) {
@@ -84,22 +92,34 @@ public class Base {
 
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-
+		
 		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(50));
 		driver.manage().window().maximize();
 		return driver;
 	}
 
-	public String getScreenShortpath(String testCaseName, WebDriver driver) {
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File source = ts.getScreenshotAs(OutputType.FILE);
-		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
-		try {
-			FileUtils.copyFile(source, new File(destinationFile));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+//	public String getScreenShortpath(String testCaseName, WebDriver driver) {
+//		TakesScreenshot ts = (TakesScreenshot) driver;
+//		File source = ts.getScreenshotAs(OutputType.FILE);
+//		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+//		try {
+//			FileUtils.copyFile(source, new File(destinationFile));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return destinationFile;
+//	}
+	public String getScreenShotPath(String testCaseName,WebDriver driver) throws IOException
+	{
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyy_hh_mm_ss");
+		//String png= System.currentTimeMillis()+ ".png";
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		File source =ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir")+"\\reports\\"+testCaseName+formater.format(calendar.getTime())+".png";
+		FileUtils.copyFile(source,new File(destinationFile));
+		
 		return destinationFile;
 	}
 }
